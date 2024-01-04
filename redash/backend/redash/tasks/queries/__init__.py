8259a10c1f18fc18619a27526b8d4613f6306f97
Alexande Mengesha <67500303+alextakele@users.noreply.github.com>
2024-01-03 19:42:53 -0800

    Delete redash directory

--- redash/backend/redash/tasks/queries/__init__.py
+++
@@ -1,8 +0,0 @@
-from .execution import enqueue_query, execute_query
-from .maintenance import (
-    cleanup_query_results,
-    empty_schedules,
-    refresh_queries,
-    refresh_schemas,
-    remove_ghost_locks,
-)
